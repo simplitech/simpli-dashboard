e769819d19b7fb2eb4875c834887574cc728048c
melanke <gil.bueno@simpli.com.br>
2023-07-10 12:48:34 -0300

    v1.4.2 - CU-86787rt1x - ClickClock: Corrigir Regex que identifica ID de issue

--- src/renderer/src/clickupServices.ts
+++ src/renderer/src/clickupServices.ts
@@ -97,7 +97,38 @@ export async function getTaskListTime(taskIds: string[], config: { clickupApiKey
 }
 
 export function clickupIdFromText(text: string): string | undefined {
-  return /(?:^|\s)(?:CU-|#)?([a-zA-Z]*\d[a-zA-Z\d]*)(?=\s|$)/g.exec(text)?.[1]
+  const regexCU = /CU-([a-z]+|\d+)+\b/g
+  const matchesCU = text.match(regexCU)
+
+  if (matchesCU && matchesCU.length > 0) {
+    return matchesCU[0].replace('CU-', '')
+  }
+
+  const regexSharp = /#([a-z]+|\d+)+\b/g
+  const matchesSharp = text.match(regexSharp)
+
+  if (matchesSharp && matchesSharp.length > 0) {
+    return matchesSharp[0].replace('#', '')
+  }
+
+  let textRefined = text
+  let matchesPure
+  let attempts = 0
+  do {
+    const regexPure = /\b([a-z]+|\d+){7,}\b/g
+    matchesPure = textRefined.match(regexPure)
+    if (matchesPure && matchesPure.length) {
+      const foundMatch = matchesPure[0].match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/g)
+      if (foundMatch) {
+        return foundMatch[0]
+      }
+
+      textRefined = textRefined.replace(matchesPure[0], '')
+    }
+    attempts++
+  } while (matchesPure && attempts < 4)
+
+  return undefined
 }
 
 export function getTaskTimeStatus(taskStatus: TaskStatus | null, statusName: string): number {
